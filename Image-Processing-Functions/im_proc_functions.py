2f3f2e5af703c630fead97276229293ddf0fdf1b
W Yandell <wayne.t.yandell@gmail.com>
2020-07-09 12:40:01 -0500

    initial package and supporting material
    first draft of functions for scraping, transforming, and outputting pandas dataframe of stats with Skin_Detector module and cascade from open cv, at least I think it's from open cv: remindme.

---
+++ Image-Processing-Functions/im_proc_functions.py
@@ -0,0 +1,142 @@
+import imageio
+import os
+import numpy as np
+from bs4 import BeautifulSoup
+import requests
+import cv2
+import pandas as pd
+from scipy import *
+import SkinDetector
+
+
+def get_filepaths(directory):
+    file_paths = []  # List which will store all of the full filepaths.
+
+    # Walk the tree.
+    if isinstance(directory, list):
+        for folder in directory:
+            for root, directories, files in os.walk(folder):
+                for filename in files:
+                    # Join the two strings in order to form the full filepath.
+                    filepath = os.path.join(root, filename)
+                    file_paths.append(filepath)  # Add it to the list.
+    else:
+        for root, directories, files in os.walk(directory):
+            for filename in files:
+                # Join the two strings in order to form the full filepath.
+                filepath = os.path.join(root, filename)
+                file_paths.append(filepath)  # Add it to the list.
+
+    return file_paths  # Self-explanatory.
+
+
+def list_images(directory):
+    image_list = []
+    if isinstance(directory, str):
+        if directory.endswith('.jpg'):
+            image_list.append(directory)
+        elif directory.endswith('/'):
+            for path in get_filepaths(directory):
+                if path.endswith('.jpg'):
+                    image_list.append(path)
+    elif isinstance(directory, list):
+        for item in directory:
+            if isinstance(item, str):
+                if item.endswith('.jpg'):
+                    image_list.append(item)
+                elif item.endswith('/'):
+                    for path in get_filepaths(item):
+                        if path.endswith('.jpg'):
+                            image_list.append(path)
+    return image_list
+
+
+def extract_image_properties(image):
+    pil_im = image
+    im_array = np.array(pil_im)
+    N = im_array.shape[1]
+    M = im_array.shape[0]
+    if len(im_array.shape) == 3:
+        color_channels = im_array.shape[2]
+    else:
+        color_channels = 'None'
+    return N, M, color_channels
+
+
+def scrape_images(site):
+    r = requests.get(site)
+    data = r.text
+    soup = BeautifulSoup(data, "lxml")
+    for link in soup.find_all('img'):
+        image = link.get('src')
+        if image.endswith('.jpg'):
+            try:
+                image_name = os.path.split(image)[1]
+                r2 = requests.get(image)
+                with open(image_name, "wb") as f:
+                    f.write(r2.content)
+                # print("got an image")
+            except:
+                try:
+                    if not image.startswith('http'):
+                        image = 'http:/' + image
+                    image_name = os.path.split(image)[1]
+                    r2 = requests.get(image)
+                    with open(image_name, "wb") as f:
+                        f.write(r2.content)
+                    # print("got an image")
+                except:
+                    print("something went wrong with this one")
+                    continue
+
+
+def detect_face(image_path, store_locally=False, output_directory=""):
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    image = cv2.imread(image_path)
+    greyscale = cv2.cvtColor(image_path, cv2.COLOR_BGR2GRAY)
+    greyscale = cv2.equalizeHist(greyscale)
+    detected_faces = face_cascade.detectMultiScale(greyscale, 1.1, 4)
+    face_images = []
+    i = 0
+    for (x, y, w, h) in detected_faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        current_face = image[y:y + h, x:x + w]
+        if store_locally:
+            if not output_directory:
+                output_directory = os.path.join(os.getcwd(), image.stem)
+            if not os.path.exists(output_directory):
+                os.mkdir(output_directory)
+            output_path = os.path.join(output_directory, '_'.join([image_path.stem, str(i)]))
+            cv2.imwrite(output_path, current_face)
+        face_images.append(current_face)
+        i += 1
+    print(f"{str(i)} faces detected in {image.stem}")
+    return face_images
+
+
+def skin_or_nothing(image_path):
+    face_images = detect_face(image_path)
+    return [SkinDetector.skin_detector.process(image) for image in face_images]
+
+
+def add_greyscale_stats(image_list, df=None):
+    if df:
+        column_names = list(df)
+    else:
+        column_names = ['average', 'min (>0)', 'max']
+        df = pd.DataFrame(columns=column_names)
+    new_data = []
+    for image in image_list:
+        greyscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cur_mean = np.true_divide(greyscale.sum(1), (greyscale != 0).sum(1))
+        min = np.min(greyscale, where=greyscale != 0)
+        max = np.max(greyscale, where=greyscale != 0)
+        zipped = zip(column_names, [cur_mean, min, max])
+        output_dict = dict(zipped)
+        new_data.append(output_dict)
+    df = df.append(new_data, True)
+    return df
+
+
+if __name__ == "__main__":
+    image_list = list_images(os.getcwd() + '/Images')
